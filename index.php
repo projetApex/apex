648558c5dd055244bbff621325ba4f39c11509b2
hugoodn <hugo.donne@ynov.com>
2023-10-26 16:34:09 +0200

    test

--- index.php
+++ index.php
@@ -1,20 +1,33 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 
 <body>
+    <?php include('header.php'); ?>
     <h1>Apex</h1>
 
+    <?php
+    // Connexion à la base de données
+    $bdd = new PDO('mysql:host=localhost;dbname=apex;charset=utf8', 'root', '');
 
-    <img src="./style/assets/spell/catalystPassive.png" alt="">
-    <img src="./style/assets/spell/catalystTactique.png" alt="">
-    <img src="./style/assets/spell/catalystUltime.png" alt="">
+    // Requête SQL pour récupérer les images
+    $query = $bdd->query("SELECT image_data FROM images");
+
+    // Boucle pour afficher les images
+    while ($row = $query->fetch()) {
+        // Affichage de l'image
+        echo '<img src="data:image/jpeg;base64,' . base64_encode($row['image_data']) . '" />';
+    }
+
+    // Fermeture de la requête
+    $query->closeCursor();
+    ?>
 
 </body>
 
