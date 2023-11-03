8841c2014b6c79e96649c1e1af5f0250ae336e84
Keil-ENZO <enzo.keil06@icloud.com>
2023-11-03 13:59:59 +0100

    modif file

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 try
 {
-	$db = new PDO('mysql:host=localhost;dbname=apex;charset=utf8', 'root', '');
+	// $db = new PDO('mysql:host=localhost;dbname=apex;charset=utf8', 'root', '');
+	$db = new PDO('mysql:host=localhost;dbname=apex;charset=utf8', 'root', 'root');
 
     $sql = 'SELECT * FROM img';
     $recipesStatement = $db->prepare($sql);
@@ -31,7 +32,7 @@ catch (Exception $e)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style1.css">
+    <link rel="stylesheet" href="./style/style2.css">
     <title>Document</title>
 </head>
 
