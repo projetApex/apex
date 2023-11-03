a9d2bd7208cbee8d028d85ed815502eb3e0be247
AxelDC1601 <axel.didiercouzard@ynov.com>
2023-11-03 16:59:39 +0100

    nom-perso

--- index.php
+++ index.php
@@ -26,6 +26,12 @@ try
     $recipesStatement3 = $db->prepare($sql3);
     $recipesStatement3->execute();
     $recipes3 = $recipesStatement3->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql4 = 'SELECT * FROM personnages';
+    $recipesStatement4 = $db->prepare($sql4);
+    $recipesStatement4->execute();
+    $recipes4 = $recipesStatement4->fetchAll(PDO::FETCH_ASSOC);
+
     
 }
 catch (Exception $e)
@@ -50,6 +56,7 @@ catch (Exception $e)
         <div class="box">
             <img class="wraith" src="<?php echo $recipes[0]['img_perso']?>" alt="">
             <div class="skill">
+                <h3 class="perso"><?php echo $recipes4[0]['name']?></h3>
                 <img class="spell" src="<?php echo $recipes2[0]['passive_img']?>" alt="">
                 <p class="name"><?php echo $recipes3[0]['passive']?></p>
                 <img class="spell" src="<?php echo $recipes2[0]['tactical_img']?>" alt="">
@@ -61,6 +68,7 @@ catch (Exception $e)
         <div class="box">
             <img class="octane" src="<?php echo $recipes[1]['img_perso']?>" alt="">
             <div class="skill">
+            <h3 class="perso"><?php echo $recipes4[1]['name']?></h3>
                 <img class="spell" src="<?php echo $recipes2[1]['passive_img']?>" alt="">
                 <p class="name"><?php echo $recipes3[1]['passive']?></p>
                 <img class="spell" src="<?php echo $recipes2[1]['tactical_img']?>" alt="">
@@ -72,6 +80,7 @@ catch (Exception $e)
         <div class="box">
             <img class="revenant" src="<?php echo $recipes[2]['img_perso']?>" alt="">
             <div class="skill">
+            <h3 class="perso"><?php echo $recipes4[2]['name']?></h3>
                 <img class="spell" src="<?php echo $recipes2[2]['passive_img']?>" alt="">
                 <p class="name"><?php echo $recipes3[2]['passive']?></p>
                 <img class="spell" src="<?php echo $recipes2[2]['tactical_img']?>" alt="">
@@ -83,6 +92,7 @@ catch (Exception $e)
         <div class="box">
             <img class="catalyst" src="<?php echo $recipes[3]['img_perso']?>" alt="">
             <div class="skill">
+            <h3 class="perso"><?php echo $recipes4[3]['name']?></h3>
                 <img class="spell" src="<?php echo $recipes2[3]['passive_img']?>" alt="">
                 <p class="name"><?php echo $recipes3[3]['passive']?></p>
                 <img class="spell" src="<?php echo $recipes2[3]['tactical_img']?>" alt="">
