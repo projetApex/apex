0b4d5c67d4b083793adea230165bd9f8a979783a
Flyax06 <gottilorenzo06@gmail.com>
2023-11-09 14:01:32 +0100

    apex

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ catch (Exception $e)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style/style2.css">
+    <link rel="stylesheet" href="./style/style.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <title>Apex Legend</title>
 </head>
