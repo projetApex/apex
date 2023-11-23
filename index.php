e3ffdcc79fd7e06008850a863274a405869a45a1
hugoodn <hugo.donne@ynov.com>
2023-11-23 14:19:48 +0100

    test

--- index.php
+++ index.php
@@ -79,24 +79,18 @@ try {
 
             <div class="box" id="<?php echo strval($UserID) ?>">
 
-
-
-             
-
-
-
                 <div class="imgchange">
-                    
-                <img class="character-image" src="<?php echo $recipes[$i]['img_perso'] ?>" alt="">
-                   <select class="image-selector global-selector" data-character-index="<?= $i - 1 ?>">
-                    <?php
-                    foreach ($recipes as $image) {
-                        echo '<option value="' . $image['img_path'] . '" > ' . $image['img_number'] . '</option>';
-                    }
-                    ?>
-                </select>
+
+                    <img class="character-image" src="<?php echo $recipes[$i]['img_perso'] ?>" alt="">
+                    <select class="image-selector global-selector" data-character-index="<?= $i - 1 ?>">
+                        <?php
+                        foreach ($recipes as $image) {
+                            echo '<option value="' . $image['img_path'] . '" > ' . $image['img_number'] . '</option>';
+                        }
+                        ?>
+                    </select>
                 </div>
-               
+
                 <div class="skill">
                     <h3 class="perso">
                         <?php echo $recipes4[$i]['name'] ?>
